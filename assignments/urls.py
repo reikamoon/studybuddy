7f789a1d2570bfe86bce45a3ac665c147f17fc53
Anjelica <ambrosio.anjelica@gmail.com>
2020-06-17 19:00:26 -0700

    users can edit and delete assignments

--- assignments/urls.py
+++ assignments/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
-from assignments.views import AssignmentDetailView, CreateAssignment
+from assignments.views import AssignmentDetailView, CreateAssignment, DeleteAssignment, UpdateAssignment
 
 urlpatterns = [
     path('', views.index, name="home"),
     path('new-assignment/', CreateAssignment.as_view(),name='new'),
     path('<str:slug>/', AssignmentDetailView.as_view(), name='assignment-details-page'),
+    path('<str:slug>/edit', UpdateAssignment.as_view(), name='edit-assignment'),
+    path('<str:slug>/delete/', DeleteAssignment.as_view(), name='delete-assignment'),
 ]
--- assignments/views.py
+++ assignments/views.py
@@ -6,6 +6,8 @@ import requests
 from django.urls import reverse_lazy
 from .models import Assignment
 from .forms import AssignmentForm
+from django.contrib import messages
+from django.views.generic.edit import DeleteView, UpdateView
 
 # Create your views here.
 def index(request):
@@ -33,3 +35,12 @@ class CreateAssignment(CreateView):
         if form.is_valid():
             post = form.save()
             return HttpResponseRedirect(reverse_lazy('home'))
+
+class UpdateAssignment(UpdateView):
+    model = Assignment
+    fields = ['assignment_name', 'assignment_link', 'description', 'due_date', 'dropbox']
+    template_name_suffix = '_update_form'
+
+class DeleteAssignment(DeleteView):
+    model = Assignment
+    success_url = "/"
